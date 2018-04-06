0cf20b7ffd2931f6e905b075a237834b44aa0815
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2018-04-06 18:00:44 -0400

    updated font

--- bash_scripts/update-font.sh
+++ bash_scripts/update-font.sh
@@ -4,3 +4,6 @@ cd ..
 
 echo "copying over font files"
 cp node_modules/@code.gov/code-gov-font/font/* assets/fonts/.
+
+echo "copying over font face code"
+cp node_modules/@code.gov/code-gov-font/css/code-gov-font-face.css assets/css/.
