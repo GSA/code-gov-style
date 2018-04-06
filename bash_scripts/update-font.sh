9a29b669debfeee61cc24dc651d7509dc09daaf3
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2018-04-06 18:03:08 -0400

    fixed bug

--- bash_scripts/update-font.sh
+++ bash_scripts/update-font.sh
@@ -3,7 +3,7 @@ echo "starting update-font"
 cd ..
 
 echo "copying over font files"
-cp node_modules/@code.gov/code-gov-font/font/* assets/fonts/.
+cp node_modules/@code.gov/code-gov-font/font/* assets/font/.
 
 echo "copying over font face code"
 cp node_modules/@code.gov/code-gov-font/css/code-gov-font-face.css assets/css/.
