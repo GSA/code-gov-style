20388ce9893667335c2e48cd93954a4e1231d475
DanielJDufour <daniel.j.dufour@gmail.com>
2018-07-03 19:47:08 +0000

    added filter box

---
+++ assets/js/filter_box.js
@@ -0,0 +1,55 @@
+'use strict';
+
+(function() {
+    class FilterBox extends HTMLElement {
+        constructor() {
+
+            // establish prototype chain
+            super();
+        }
+
+
+        // fires after the element has been attached to the DOM
+        connectedCallback() {
+            // creating a container for the editable-list component
+            const container = document.createElement('div');
+          
+            this.title = this.getAttribute('title');
+            this.options = JSON.parse(this.getAttribute('options')).map(option => {
+              return { name: option, value: option, selected: false };
+            });
+            console.log("options:", this.options);
+
+            container.className = "filter-box";
+            
+            container.innerHTML = this.getHTML(); 
+
+            // appending the container to the shadow DOM
+            this.appendChild(container);            
+            
+            /*
+            let removeElementButtons = [...this.shadowRoot.querySelectorAll('.editable-list-remove-item')];
+            let addElementButton = this.shadowRoot.querySelector('.editable-list-add-item');
+
+            this.itemList = this.shadowRoot.querySelector('.item-list');
+
+            this.handleRemoveItemListeners(removeElementButtons);
+            addElementButton.addEventListener('click', this.addListItem, false);
+            */
+        }
+        
+        getHTML() {
+          return `
+            <div class="title">${this.title}</div>
+            <ul class="options">
+              ${this.options.map(option => {
+                return '<li><input type="checkbox" id="' + option.value + '" value="' + option.value + '"><label for="' + option.value + '"><span>' + option.name + '</span></label></li>';
+              }).join("\n")}
+            </ul>
+          `;          
+        }
+    }
+
+    // let the browser know about the custom element
+    customElements.define('filter-box', FilterBox);
+})();
---
+++ components/filter.md
@@ -0,0 +1,21 @@
+---
+layout: components
+title: "Filter"
+---
+
+<script src="{{ '/assets/js/filter_box.js' | relative_url }}"></script>
+
+
+<p>Below you will find the buttons used by code.gov</p>
+
+# Normal Button
+<filter-box
+  title="Organization Type"
+  options='["Federal Government","State Government","Local Government"]'
+></filter-box>
+```
+<filter-box
+  title="Organization Type"
+  options="['Federal Government', 'State Government', 'Local Government']"
+></filter-box>
+```
