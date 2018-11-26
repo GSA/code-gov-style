02822ed954177106a7f34917f353cbc5ea70ba07
DanielJDufour <daniel.j.dufour@gmail.com>
2018-11-26 20:16:29 +0000

    subsetted tt lakes to just US ASCII

--- bash_scripts/update-font.sh
+++ bash_scripts/update-font.sh
@@ -10,5 +10,10 @@ cp node_modules/@code.gov/code-gov-font/font/* assets/font/.
 echo "copying over font face code"
 cp node_modules/@code.gov/code-gov-font/css/code-gov-font-face.css assets/css/.
 
+echo "subsetting TT Lakes Font"
+cd assets/font
+node ../../node_modules/glyphhanger/cmd.js --subset=*webfont.ttf --US_ASCII
+
 printf '\e[1;31m%-6s\e[m' "please update _sass/_fontello.scss"
 echo ""
+
