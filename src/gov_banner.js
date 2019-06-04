bb01fa628fe24d8afa43b68941a72c724f571fae
hendersonanalytics <everett@hendersonanalytics.com>
2019-06-04 11:56:37 -0500

    rework svg data uris

--- src/gov_banner.js
+++ src/gov_banner.js
@@ -17,6 +17,10 @@
       this.update();
     }
 
+    getSvgUri(svgString) {
+      return `data:image/svg+xml;utf8,${svgString}`;
+    }
+
     // attributeChangedCallback(attrName, oldVal, newVal) {
     //   if (attrName === 'options') {
     //     this.update();
@@ -25,8 +29,13 @@
 
     update() {
       const smallUsFlagURI = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAG1BMVEUdM7EeNLIeM7HgQCDaPh/bPh/bPx/////bPyBEby41AAAAUElEQVQI123MNw4CABDEwD3jC/9/MQ1BQrgeOSkIqYe2o2FZtthXgQLgbHVMZdlsfUQFQnHtjP1+8BUhBDKOqtmfot6ojqPzR7TjdU+f6vkED+IDPhTBcMAAAAAASUVORK5CYII=";
-      const dotGovIconURI = "data:image/svg+xml,%3Csvg id='Layer_1' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 54 54'%3E%3Cdefs%3E%3Cstyle%3E.cls-1%7Bfill:%232378c3;%7D.cls-2%7Bfill:none;stroke:%23005ea2;stroke-miterlimit:10;%7D%3C/style%3E%3C/defs%3E%3Ctitle%3Edot gov icon%3C/title%3E%3Cpath class='cls-1' d='M36.5,20.91v1.36H35.15a0.71,0.71,0,0,1-.73.68H18.23a0.71,0.71,0,0,1-.73-0.68H16.14V20.91l10.18-4.07Zm0,13.57v1.36H16.14V34.48a0.71,0.71,0,0,1,.73-0.68h18.9A0.71,0.71,0,0,1,36.5,34.48ZM21.57,23.62v8.14h1.36V23.62h2.71v8.14H27V23.62h2.71v8.14h1.36V23.62h2.71v8.14h0.63a0.71,0.71,0,0,1,.73.68v0.68H17.5V32.45a0.71,0.71,0,0,1,.73-0.68h0.63V23.62h2.71Z'/%3E%3Ccircle class='cls-2' cx='27' cy='27.12' r='26'/%3E%3C/svg%3E";
-      const httpsIconURI = "data:image/svg+xml,%3Csvg id='Layer_1' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 54 54'%3E%3Cdefs%3E%3Cstyle%3E.cls-1%7Bfill:%23719f2a;%7D.cls-2%7Bfill:none;stroke:%23538200;stroke-miterlimit:10;%7D%3C/style%3E%3C/defs%3E%3Ctitle%3Ehttps icon%3C/title%3E%3Cpath class='cls-1' d='M34.72,34.84a1.29,1.29,0,0,1-1.29,1.29H20.57a1.29,1.29,0,0,1-1.29-1.29V27.12a1.29,1.29,0,0,1,1.29-1.29H21V23.26a6,6,0,0,1,12,0v2.57h0.43a1.29,1.29,0,0,1,1.29,1.29v7.72Zm-4.29-9V23.26a3.43,3.43,0,0,0-6.86,0v2.57h6.86Z'/%3E%3Ccircle class='cls-2' cx='27' cy='27.12' r='26'/%3E%3C/svg%3E";
+
+      const dotGovIconSvgString = '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54 54"><defs><style>.cls-1{fill:#2378c3;}.cls-2{fill:none;stroke:#005ea2;stroke-miterlimit:10;}</style></defs><title>dot gov icon</title><path class="cls-1" d="M36.5,20.91v1.36H35.15a0.71,0.71,0,0,1-.73.68H18.23a0.71,0.71,0,0,1-.73-0.68H16.14V20.91l10.18-4.07Zm0,13.57v1.36H16.14V34.48a0.71,0.71,0,0,1,.73-0.68h18.9A0.71,0.71,0,0,1,36.5,34.48ZM21.57,23.62v8.14h1.36V23.62h2.71v8.14H27V23.62h2.71v8.14h1.36V23.62h2.71v8.14h0.63a0.71,0.71,0,0,1,.73.68v0.68H17.5V32.45a0.71,0.71,0,0,1,.73-0.68h0.63V23.62h2.71Z"/><circle class="cls-2" cx="27" cy="27.12" r="26"/></svg>';
+
+      const httpsIconSvgString = '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54 54"><defs><style>.cls-1{fill:#719f2a;}.cls-2{fill:none;stroke:#538200;stroke-miterlimit:10;}</style></defs><title>https icon</title><path class="cls-1" d="M34.72,34.84a1.29,1.29,0,0,1-1.29,1.29H20.57a1.29,1.29,0,0,1-1.29-1.29V27.12a1.29,1.29,0,0,1,1.29-1.29H21V23.26a6,6,0,0,1,12,0v2.57h0.43a1.29,1.29,0,0,1,1.29,1.29v7.72Zm-4.29-9V23.26a3.43,3.43,0,0,0-6.86,0v2.57h6.86Z"/><circle class="cls-2" cx="27" cy="27.12" r="26"/></svg>';
+
+      const dotGovIconURI = this.getSvgUri(dotGovIconSvgString);
+      const httpsIconURI = this.getSvgUri(httpsIconSvgString);
 
       this.innerHTML = `
       <div class="usa-banner">
