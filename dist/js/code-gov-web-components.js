06c6b48925bcdb994105cdfbe89816b115b5b7cb
hendersonanalytics <everett@hendersonanalytics.com>
2019-06-04 13:33:09 -0500

    change require to an import statement

--- dist/js/code-gov-web-components.js
+++ dist/js/code-gov-web-components.js
@@ -358,6 +358,8 @@ function (_HTMLElement) {
 customElements.define('filter-tag', FilterTag);
 'use strict';
 
+require("uswds");
+
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -383,8 +385,6 @@ function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[nat
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-require('uswds');
 
 (function () {
   /*global HTMLElement*/
--- src/gov_banner.js
+++ src/gov_banner.js
@@ -1,5 +1,5 @@
 'use strict';
-require('uswds');
+import 'uswds';
 
 
 (function() {
