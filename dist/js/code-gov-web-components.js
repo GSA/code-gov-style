456fd19be3aee8b5602699d93627ae1bbc82d024
hendersonanalytics <everett@hendersonanalytics.com>
2019-06-05 10:45:18 -0500

    move event handlers and such to the constructor

--- dist/js/code-gov-web-components.js
+++ dist/js/code-gov-web-components.js
@@ -392,42 +392,61 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
     _inherits(GovBanner, _HTMLElement);
 
     function GovBanner() {
+      var _this;
+
       _classCallCheck(this, GovBanner);
 
       // establish prototype chain
-      return _possibleConstructorReturn(this, _getPrototypeOf(GovBanner).call(this)); // this.handleClick = this.handleClick.bind(this);
+      _this = _possibleConstructorReturn(this, _getPrototypeOf(GovBanner).call(this));
+
+      _this.render();
+
+      _this.buttonToggle = _this.querySelector('button.usa-accordion__button');
+      _this.bannerHeader = _this.querySelector('header.usa-banner__header');
+      _this.accordionContent = _this.querySelector('div.usa-banner__content');
+
+      _this.buttonToggle.addEventListener('click', function (event) {
+        console.log('event', event);
+        console.log('i got clicked!');
+
+        if (_this.buttonToggle.getAttribute('aria-expanded') === 'true') {
+          _this.collapseBanner();
+        } else {
+          _this.expandBanner();
+        }
+      }); // this.handleClick = this.handleClick.bind(this);
+
+
+      return _this;
     } // static get observedAttributes() {
     //   return ['image', 'title'];
     // }
     // fires after the element has been attached to the DOM
+    // connectedCallback() {
+    //   this.render();
+    //   this.buttonToggle = this.querySelector('button.usa-accordion__button');
+    //   this.bannerHeader = this.querySelector('header.usa-banner__header');
+    //   this.accordionContent = this.querySelector('div.usa-banner__content');
+    //
+    //   this.buttonToggle.addEventListener('click', (event) => {
+    //     console.log('event', event);
+    //     console.log('i got clicked!');
+    //     if (this.buttonToggle.getAttribute('aria-expanded') === 'true') {
+    //       this.collapseBanner();
+    //     } else {
+    //       this.expandBanner();
+    //     }
+    //   });
+    // }
 
 
     _createClass(GovBanner, [{
-      key: "connectedCallback",
-      value: function connectedCallback() {
-        var _this = this;
-
-        this.update();
-        this.buttonToggle = this.querySelector('button.usa-accordion__button');
-        this.bannerHeader = this.querySelector('header.usa-banner__header');
-        this.accordionContent = this.querySelector('div.usa-banner__content');
-        this.buttonToggle.addEventListener('click', function () {
-          console.log('i got clicked!');
-
-          if (_this.buttonToggle.getAttribute('aria-expanded') === 'true') {
-            _this.collaspseBanner();
-          } else {
-            _this.expandBanner();
-          }
-        });
-      }
-    }, {
       key: "getSvgUri",
       value: function getSvgUri(svgString) {
         return "data:image/svg+xml;base64,".concat(svgString);
       } // attributeChangedCallback(attrName, oldVal, newVal) {
       //   if (attrName === 'options') {
-      //     this.update();
+      //     this.render();
       //   }
       // }
       // disconnectedCallback() {
@@ -454,15 +473,15 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
         this.bannerHeader.classlist.add('usa-banner__header--expanded');
       }
     }, {
-      key: "collaspseBanner",
-      value: function collaspseBanner() {
+      key: "collapseBanner",
+      value: function collapseBanner() {
         this.accordionContent.setAttribute('hidden', '');
         this.buttonToggle.setAttribute('aria-expanded', 'false');
         this.bannerHeader.classlist.remove('usa-banner__header--expanded');
       }
     }, {
-      key: "update",
-      value: function update() {
+      key: "render",
+      value: function render() {
         var smallUsFlagURI = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAG1BMVEUdM7EeNLIeM7HgQCDaPh/bPh/bPx/////bPyBEby41AAAAUElEQVQI123MNw4CABDEwD3jC/9/MQ1BQrgeOSkIqYe2o2FZtthXgQLgbHVMZdlsfUQFQnHtjP1+8BUhBDKOqtmfot6ojqPzR7TjdU+f6vkED+IDPhTBcMAAAAAASUVORK5CYII=";
         var dotGovIconSvgString = 'PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1NCA1NCI+PGRlZnM+PHN0eWxlPi5jbHMtMXtmaWxsOiMyMzc4YzM7fS5jbHMtMntmaWxsOm5vbmU7c3Ryb2tlOiMwMDVlYTI7c3Ryb2tlLW1pdGVybGltaXQ6MTA7fTwvc3R5bGU+PC9kZWZzPjx0aXRsZT5kb3QgZ292IGljb248L3RpdGxlPjxwYXRoIGNsYXNzPSJjbHMtMSIgZD0iTTM2LjUsMjAuOTF2MS4zNkgzNS4xNWEwLjcxLDAuNzEsMCwwLDEtLjczLjY4SDE4LjIzYTAuNzEsMC43MSwwLDAsMS0uNzMtMC42OEgxNi4xNFYyMC45MWwxMC4xOC00LjA3Wm0wLDEzLjU3djEuMzZIMTYuMTRWMzQuNDhhMC43MSwwLjcxLDAsMCwxLC43My0wLjY4aDE4LjlBMC43MSwwLjcxLDAsMCwxLDM2LjUsMzQuNDhaTTIxLjU3LDIzLjYydjguMTRoMS4zNlYyMy42MmgyLjcxdjguMTRIMjdWMjMuNjJoMi43MXY4LjE0aDEuMzZWMjMuNjJoMi43MXY4LjE0aDAuNjNhMC43MSwwLjcxLDAsMCwxLC43My42OHYwLjY4SDE3LjVWMzIuNDVhMC43MSwwLjcxLDAsMCwxLC43My0wLjY4aDAuNjNWMjMuNjJoMi43MVoiLz48Y2lyY2xlIGNsYXNzPSJjbHMtMiIgY3g9IjI3IiBjeT0iMjcuMTIiIHI9IjI2Ii8+PC9zdmc+';
         var httpsIconSvgString = 'PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1NCA1NCI+PGRlZnM+PHN0eWxlPi5jbHMtMXtmaWxsOiM3MTlmMmE7fS5jbHMtMntmaWxsOm5vbmU7c3Ryb2tlOiM1MzgyMDA7c3Ryb2tlLW1pdGVybGltaXQ6MTA7fTwvc3R5bGU+PC9kZWZzPjx0aXRsZT5odHRwcyBpY29uPC90aXRsZT48cGF0aCBjbGFzcz0iY2xzLTEiIGQ9Ik0zNC43MiwzNC44NGExLjI5LDEuMjksMCwwLDEtMS4yOSwxLjI5SDIwLjU3YTEuMjksMS4yOSwwLDAsMS0xLjI5LTEuMjlWMjcuMTJhMS4yOSwxLjI5LDAsMCwxLDEuMjktMS4yOUgyMVYyMy4yNmE2LDYsMCwwLDEsMTIsMHYyLjU3aDAuNDNhMS4yOSwxLjI5LDAsMCwxLDEuMjksMS4yOXY3LjcyWm0tNC4yOS05VjIzLjI2YTMuNDMsMy40MywwLDAsMC02Ljg2LDB2Mi41N2g2Ljg2WiIvPjxjaXJjbGUgY2xhc3M9ImNscy0yIiBjeD0iMjciIGN5PSIyNy4xMiIgcj0iMjYiLz48L3N2Zz4=';
@@ -473,13 +492,13 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
     }]);
 
     return GovBanner;
-  }(_wrapNativeSuper(HTMLElement)); // let the browser know about the custom element
+  }(_wrapNativeSuper(HTMLElement));
+})(); // let the browser know about the custom element
 
-  /*global customElements*/
+/*global customElements*/
 
 
-  customElements.define('gov-banner', GovBanner);
-})();
+customElements.define('gov-banner', GovBanner);
 "use strict";
 'use strict';
 
--- src/gov_banner.js
+++ src/gov_banner.js
@@ -5,6 +5,20 @@
     constructor() {
         // establish prototype chain
         super();
+        this.render();
+        this.buttonToggle = this.querySelector('button.usa-accordion__button');
+        this.bannerHeader = this.querySelector('header.usa-banner__header');
+        this.accordionContent = this.querySelector('div.usa-banner__content');
+
+        this.buttonToggle.addEventListener('click', (event) => {
+          console.log('event', event);
+          console.log('i got clicked!');
+          if (this.buttonToggle.getAttribute('aria-expanded') === 'true') {
+            this.collapseBanner();
+          } else {
+            this.expandBanner();
+          }
+        });
         // this.handleClick = this.handleClick.bind(this);
     }
 
@@ -13,21 +27,22 @@
     // }
 
     // fires after the element has been attached to the DOM
-    connectedCallback() {
-      this.update();
-      this.buttonToggle = this.querySelector('button.usa-accordion__button');
-      this.bannerHeader = this.querySelector('header.usa-banner__header');
-      this.accordionContent = this.querySelector('div.usa-banner__content');
-
-      this.buttonToggle.addEventListener('click', () => {
-        console.log('i got clicked!');
-        if (this.buttonToggle.getAttribute('aria-expanded') === 'true') {
-          this.collaspseBanner();
-        } else {
-          this.expandBanner();
-        }
-      });
-    }
+    // connectedCallback() {
+    //   this.render();
+    //   this.buttonToggle = this.querySelector('button.usa-accordion__button');
+    //   this.bannerHeader = this.querySelector('header.usa-banner__header');
+    //   this.accordionContent = this.querySelector('div.usa-banner__content');
+    //
+    //   this.buttonToggle.addEventListener('click', (event) => {
+    //     console.log('event', event);
+    //     console.log('i got clicked!');
+    //     if (this.buttonToggle.getAttribute('aria-expanded') === 'true') {
+    //       this.collapseBanner();
+    //     } else {
+    //       this.expandBanner();
+    //     }
+    //   });
+    // }
 
     getSvgUri(svgString) {
       return `data:image/svg+xml;base64,${svgString}`;
@@ -35,7 +50,7 @@
 
     // attributeChangedCallback(attrName, oldVal, newVal) {
     //   if (attrName === 'options') {
-    //     this.update();
+    //     this.render();
     //   }
     // }
 
@@ -62,13 +77,13 @@
       this.bannerHeader.classlist.add('usa-banner__header--expanded');
     }
 
-    collaspseBanner() {
+    collapseBanner() {
       this.accordionContent.setAttribute('hidden', '');
       this.buttonToggle.setAttribute('aria-expanded', 'false');
       this.bannerHeader.classlist.remove('usa-banner__header--expanded');
     }
 
-    update() {
+    render() {
       const smallUsFlagURI = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAG1BMVEUdM7EeNLIeM7HgQCDaPh/bPh/bPx/////bPyBEby41AAAAUElEQVQI123MNw4CABDEwD3jC/9/MQ1BQrgeOSkIqYe2o2FZtthXgQLgbHVMZdlsfUQFQnHtjP1+8BUhBDKOqtmfot6ojqPzR7TjdU+f6vkED+IDPhTBcMAAAAAASUVORK5CYII=";
 
       const dotGovIconSvgString = 'PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1NCA1NCI+PGRlZnM+PHN0eWxlPi5jbHMtMXtmaWxsOiMyMzc4YzM7fS5jbHMtMntmaWxsOm5vbmU7c3Ryb2tlOiMwMDVlYTI7c3Ryb2tlLW1pdGVybGltaXQ6MTA7fTwvc3R5bGU+PC9kZWZzPjx0aXRsZT5kb3QgZ292IGljb248L3RpdGxlPjxwYXRoIGNsYXNzPSJjbHMtMSIgZD0iTTM2LjUsMjAuOTF2MS4zNkgzNS4xNWEwLjcxLDAuNzEsMCwwLDEtLjczLjY4SDE4LjIzYTAuNzEsMC43MSwwLDAsMS0uNzMtMC42OEgxNi4xNFYyMC45MWwxMC4xOC00LjA3Wm0wLDEzLjU3djEuMzZIMTYuMTRWMzQuNDhhMC43MSwwLjcxLDAsMCwxLC43My0wLjY4aDE4LjlBMC43MSwwLjcxLDAsMCwxLDM2LjUsMzQuNDhaTTIxLjU3LDIzLjYydjguMTRoMS4zNlYyMy42MmgyLjcxdjguMTRIMjdWMjMuNjJoMi43MXY4LjE0aDEuMzZWMjMuNjJoMi43MXY4LjE0aDAuNjNhMC43MSwwLjcxLDAsMCwxLC43My42OHYwLjY4SDE3LjVWMzIuNDVhMC43MSwwLjcxLDAsMCwxLC43My0wLjY4aDAuNjNWMjMuNjJoMi43MVoiLz48Y2lyY2xlIGNsYXNzPSJjbHMtMiIgY3g9IjI3IiBjeT0iMjcuMTIiIHI9IjI2Ii8+PC9zdmc+';
@@ -122,8 +137,8 @@
       `;
     }
   }
-
-  // let the browser know about the custom element
-  /*global customElements*/
-  customElements.define('gov-banner', GovBanner);
 })();
+
+// let the browser know about the custom element
+/*global customElements*/
+customElements.define('gov-banner', GovBanner);
