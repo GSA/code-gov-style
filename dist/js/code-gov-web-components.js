2c10179e874f15dd53cf2e7cbc11ffb2ed7ee46d
hendersonanalytics <everett@hendersonanalytics.com>
2019-06-10 14:12:00 -0500

    remove gov-banner scss

--- dist/js/code-gov-web-components.js
+++ dist/js/code-gov-web-components.js
@@ -388,7 +388,6 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
   var smallUsFlagURI = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAG1BMVEUdM7EeNLIeM7HgQCDaPh/bPh/bPx/////bPyBEby41AAAAUElEQVQI123MNw4CABDEwD3jC/9/MQ1BQrgeOSkIqYe2o2FZtthXgQLgbHVMZdlsfUQFQnHtjP1+8BUhBDKOqtmfot6ojqPzR7TjdU+f6vkED+IDPhTBcMAAAAAASUVORK5CYII=";
   var dotGovIconURI = 'data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1NCA1NCI+PGRlZnM+PHN0eWxlPi5jbHMtMXtmaWxsOiMyMzc4YzM7fS5jbHMtMntmaWxsOm5vbmU7c3Ryb2tlOiMwMDVlYTI7c3Ryb2tlLW1pdGVybGltaXQ6MTA7fTwvc3R5bGU+PC9kZWZzPjx0aXRsZT5kb3QgZ292IGljb248L3RpdGxlPjxwYXRoIGNsYXNzPSJjbHMtMSIgZD0iTTM2LjUsMjAuOTF2MS4zNkgzNS4xNWEwLjcxLDAuNzEsMCwwLDEtLjczLjY4SDE4LjIzYTAuNzEsMC43MSwwLDAsMS0uNzMtMC42OEgxNi4xNFYyMC45MWwxMC4xOC00LjA3Wm0wLDEzLjU3djEuMzZIMTYuMTRWMzQuNDhhMC43MSwwLjcxLDAsMCwxLC43My0wLjY4aDE4LjlBMC43MSwwLjcxLDAsMCwxLDM2LjUsMzQuNDhaTTIxLjU3LDIzLjYydjguMTRoMS4zNlYyMy42MmgyLjcxdjguMTRIMjdWMjMuNjJoMi43MXY4LjE0aDEuMzZWMjMuNjJoMi43MXY4LjE0aDAuNjNhMC43MSwwLjcxLDAsMCwxLC43My42OHYwLjY4SDE3LjVWMzIuNDVhMC43MSwwLjcxLDAsMCwxLC43My0wLjY4aDAuNjNWMjMuNjJoMi43MVoiLz48Y2lyY2xlIGNsYXNzPSJjbHMtMiIgY3g9IjI3IiBjeT0iMjcuMTIiIHI9IjI2Ii8+PC9zdmc+';
   var httpsIconURI = 'data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1NCA1NCI+PGRlZnM+PHN0eWxlPi5jbHMtMXtmaWxsOiM3MTlmMmE7fS5jbHMtMntmaWxsOm5vbmU7c3Ryb2tlOiM1MzgyMDA7c3Ryb2tlLW1pdGVybGltaXQ6MTA7fTwvc3R5bGU+PC9kZWZzPjx0aXRsZT5odHRwcyBpY29uPC90aXRsZT48cGF0aCBjbGFzcz0iY2xzLTEiIGQ9Ik0zNC43MiwzNC44NGExLjI5LDEuMjksMCwwLDEtMS4yOSwxLjI5SDIwLjU3YTEuMjksMS4yOSwwLDAsMS0xLjI5LTEuMjlWMjcuMTJhMS4yOSwxLjI5LDAsMCwxLDEuMjktMS4yOUgyMVYyMy4yNmE2LDYsMCwwLDEsMTIsMHYyLjU3aDAuNDNhMS4yOSwxLjI5LDAsMCwxLDEuMjksMS4yOXY3LjcyWm0tNC4yOS05VjIzLjI2YTMuNDMsMy40MywwLDAsMC02Ljg2LDB2Mi41N2g2Ljg2WiIvPjxjaXJjbGUgY2xhc3M9ImNscy0yIiBjeD0iMjciIGN5PSIyNy4xMiIgcj0iMjYiLz48L3N2Zz4=';
-  var themeStyleString = "\n    .usa-banner {\n      background-color: #323a45;\n    }\n\n    .usa-banner__header-text,\n    .usa-media-block__body {\n      color: #ffffff;\n    }\n  ";
   /*global HTMLElement*/
 
   var GovBanner =
@@ -403,14 +402,10 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 
       // establish prototype chain
       _this = _possibleConstructorReturn(this, _getPrototypeOf(GovBanner).call(this));
-      _this.toggleAccordion = _this.toggleAccordion.bind(_assertThisInitialized(_this)); // this.updateTheme();
-
+      _this.toggleAccordion = _this.toggleAccordion.bind(_assertThisInitialized(_this));
       _this.isCollapsed = true;
       return _this;
-    } // static get observedAttributes() {
-    //   return ['dark'];
-    // }
-    // fires after the element has been attached to the DOM
+    } // fires after the element has been attached to the DOM
 
 
     _createClass(GovBanner, [{
@@ -426,11 +421,7 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
         this.bannerHeader = this.querySelector('header.usa-banner__header');
         this.accordionContent = this.querySelector('div.usa-banner__content');
         this.buttonToggle.addEventListener('click', this.toggleAccordion);
-      } // updateTheme() {
-      //   this.themeStyleString = this.hasAttribute('dark') && ['', true, 'true', 'True'].includes(this.getAttribute('dark'))
-      //     ? themeStyleString : '';
-      // }
-
+      }
     }, {
       key: "toggleAccordion",
       value: function toggleAccordion() {
@@ -460,14 +451,7 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
         this.buttonToggle.setAttribute('aria-expanded', 'false');
         this.bannerHeader.classList.remove('usa-banner__header--expanded');
         this.isCollapsed = true;
-      } // attributeChangedCallback(attrName) {
-      //   if (attrName === 'dark') {
-      //     this.updateTheme();
-      //     this.render();
-      //     this.postRenderActions();
-      //   }
-      // }
-
+      }
     }, {
       key: "render",
       value: function render() {
--- src/gov_banner.js
+++ src/gov_banner.js
@@ -6,30 +6,14 @@
 
   const httpsIconURI = 'data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1NCA1NCI+PGRlZnM+PHN0eWxlPi5jbHMtMXtmaWxsOiM3MTlmMmE7fS5jbHMtMntmaWxsOm5vbmU7c3Ryb2tlOiM1MzgyMDA7c3Ryb2tlLW1pdGVybGltaXQ6MTA7fTwvc3R5bGU+PC9kZWZzPjx0aXRsZT5odHRwcyBpY29uPC90aXRsZT48cGF0aCBjbGFzcz0iY2xzLTEiIGQ9Ik0zNC43MiwzNC44NGExLjI5LDEuMjksMCwwLDEtMS4yOSwxLjI5SDIwLjU3YTEuMjksMS4yOSwwLDAsMS0xLjI5LTEuMjlWMjcuMTJhMS4yOSwxLjI5LDAsMCwxLDEuMjktMS4yOUgyMVYyMy4yNmE2LDYsMCwwLDEsMTIsMHYyLjU3aDAuNDNhMS4yOSwxLjI5LDAsMCwxLDEuMjksMS4yOXY3LjcyWm0tNC4yOS05VjIzLjI2YTMuNDMsMy40MywwLDAsMC02Ljg2LDB2Mi41N2g2Ljg2WiIvPjxjaXJjbGUgY2xhc3M9ImNscy0yIiBjeD0iMjciIGN5PSIyNy4xMiIgcj0iMjYiLz48L3N2Zz4=';
 
-  const themeStyleString = `
-    .usa-banner {
-      background-color: #323a45;
-    }
-
-    .usa-banner__header-text,
-    .usa-media-block__body {
-      color: #ffffff;
-    }
-  `;
-
   /*global HTMLElement*/
   class GovBanner extends HTMLElement {
     constructor() {
         // establish prototype chain
         super();
         this.toggleAccordion = this.toggleAccordion.bind(this);
-        // this.updateTheme();
         this.isCollapsed = true;
     }
-
-    // static get observedAttributes() {
-    //   return ['dark'];
-    // }
 
     // fires after the element has been attached to the DOM
     connectedCallback() {
@@ -43,11 +27,6 @@
       this.accordionContent = this.querySelector('div.usa-banner__content');
       this.buttonToggle.addEventListener('click', this.toggleAccordion);
     }
-
-    // updateTheme() {
-    //   this.themeStyleString = this.hasAttribute('dark') && ['', true, 'true', 'True'].includes(this.getAttribute('dark'))
-    //     ? themeStyleString : '';
-    // }
 
     toggleAccordion() {
       if (this.buttonToggle.getAttribute('aria-expanded') === 'true') {
@@ -74,14 +53,6 @@
       this.bannerHeader.classList.remove('usa-banner__header--expanded');
       this.isCollapsed = true;
     }
-
-    // attributeChangedCallback(attrName) {
-    //   if (attrName === 'dark') {
-    //     this.updateTheme();
-    //     this.render();
-    //     this.postRenderActions();
-    //   }
-    // }
 
     render() {
       const { isCollapsed } = this;
